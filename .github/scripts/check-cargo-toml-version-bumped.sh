97d8365e2a273f882fe17ebcf0956a5106eeab69
Marcin <marcin.radecki@cardinals.cc>
2022-01-11 15:22:33 +0100

    Testing workflowchanges as if they were on main already

---
+++ .github/scripts/check-cargo-toml-version-bumped.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if [ -e $(git diff HEAD~ -- src/) ]; then
+  echo "No changes in the code."
+  exit 0
+fi
+
+if [ -z "$(git diff HEAD~ -- Cargo.toml | grep -q '^+version =')" ]; then
+  echo "None of commits in this PR has changed version in Cargo.toml!"
+  exit 1
+fi
