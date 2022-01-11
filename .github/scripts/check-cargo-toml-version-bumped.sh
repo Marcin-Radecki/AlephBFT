d23c80a14e1072c21de9c30358ab86c265b872ec
Marcin <marcin.radecki@cardinals.cc>
2022-01-11 15:38:16 +0100

    bumped version

--- .github/scripts/check-cargo-toml-version-bumped.sh
+++ .github/scripts/check-cargo-toml-version-bumped.sh
@@ -7,7 +7,7 @@ if [ -e "$(git diff HEAD~ -- src/)" ]; then
   exit 0
 fi
 
-if [ -z "$(git diff HEAD~ -- Cargo.toml | grep -q '^+version =')" ]; then
+if [ ! -e "$(git diff HEAD~ -- Cargo.toml | grep -q '^+version =')" ]; then
   echo "None of commits in this PR has changed version in Cargo.toml!"
   exit 1
 fi
